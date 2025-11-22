c2ca423c45318f5897891d15faf2e9a57278105d
DJ <sapzildj@gmail.com>
2025-11-22 17:47:13 +0900

    Initial Commit

---
+++ com.sapzildj.tca.reducer.conflict/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  com.sapzildj.tca.reducer.conflict
+//
+//  Created by Dongjin Suh on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ com.sapzildj.tca.reducer.conflict/com_sapzildj_tca_reducer_conflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_sapzildj_tca_reducer_conflictApp.swift
+//  com.sapzildj.tca.reducer.conflict
+//
+//  Created by Dongjin Suh on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct com_sapzildj_tca_reducer_conflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
